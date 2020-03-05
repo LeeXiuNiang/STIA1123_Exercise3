c1d7697bca66d1d6a29903a328c047239588a9f8
Lee Xiu Niang <55247305+LeeXiuNiang@users.noreply.github.com>
2020-03-05 18:34:23 +0800

    Create Q7.java

---
+++ Q7.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Q7
+        String text1 = "School of Computing";
+        String text2 = "College of Arts and Sciences";
+        String text3 = "UUM Sintok";
+        //a
+        System.out.println("Length of \"" + text1 + "\" : " + text1.length());
+        System.out.println("Length of \"" + text2 + "\" : " + text2.length());
+        System.out.println("Length of \"" + text3 + "\" : " + text3.length());
+        System.out.println("Total length: " + (text1.length()+text2.length()+text3.length()));
+        //b
+        System.out.println(text2.substring(0,20) + text1.substring(10,19));
+    }
+}
