0755cdc96743883268d7bcc25ed705b2223d3aee
Lee Xiu Niang <55247305+LeeXiuNiang@users.noreply.github.com>
2020-03-05 18:43:56 +0800

    Create Q1.java

---
+++ Q1.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    //Q1
+        String object1 = "1st way to create new object";
+        String object2 = new String("2nd way to create new object");
+    }
+}
