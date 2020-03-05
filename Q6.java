7cd818f26e4c0fb01f26b2d0a845f87473d74159
Lee Xiu Niang <55247305+LeeXiuNiang@users.noreply.github.com>
2020-03-05 18:37:55 +0800

    Create Q6.java

---
+++ Q6.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Q6
+        //a
+        Scanner input = new Scanner(System.in);
+        //b
+        System.out.println("Enter 2 integer: ");
+        int x = input.nextInt();
+        int y = input.nextInt();
+        //c
+        System.out.println("Product of " + x + " and "+ y + ": " + Math.multiplyExact(x,y));
+    }
+}
