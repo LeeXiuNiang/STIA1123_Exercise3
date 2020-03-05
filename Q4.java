bb13083d7cdb5c41a167186f7378408c6dc2ac44
Lee Xiu Niang <55247305+LeeXiuNiang@users.noreply.github.com>
2020-03-05 18:40:57 +0800

    Create Q4.java

---
+++ Q4.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Q4
+        Random rand = new Random();
+
+        int num1 = rand.nextInt(100);
+        int num2 = rand.nextInt(100);
+        int largest = Math.max(num1,num2);
+        System.out.println("Num1 : "+num1);
+        System.out.println("Num2 : "+num2);
+        System.out.println("Larger value: "+ largest);
+    }
+}
